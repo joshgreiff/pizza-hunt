36381a347608afd2ca10788d2f29a5c6a8a2b4db
Josh Greiff <josh@greiff.org>
2022-06-14 14:28:10 -0400

    checkpoint

--- models/Comment.js
+++ models/Comment.js
@@ -1,4 +1,5 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model, Types } = require('mongoose')
+const dateFormat = require('../utils/dateFormat')
 
 const CommentSchema = new Schema({
     writtenBy: {
@@ -9,9 +10,49 @@ const CommentSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
-    }
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    replies:[ReplySchema]
+},
+{
+    toJSON: {
+        getters: true,
+        virtuals: true
+    },
+    id: false
+}
+)
+
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length
 })
+
+const ReplySchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment_id
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+)
 
 const Comment = model('Comment', CommentSchema)
 
